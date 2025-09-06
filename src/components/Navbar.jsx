96a97d96de58ddc2f7bb5cdabde55596d5c1afdc
UnicornDad8 <cecibenitezca@gmail.com>
2025-09-06 16:26:32 -0300

    Add Hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,11 +23,11 @@ const Navbar = () => {
     <LinkScroll
       onClick={() => setIsOpen(false)}
       to={title}
-      offset={-100}
+      offset={-250}
       spy
       smooth
       activeClass="text-primary"
-      className="transition-colors duration-500 cursor-pointer text-3xl my-6 lg:my-0 lg:text-lg text-black/60 hover:text-black"
+      className="my-6 text-3xl transition-colors duration-500 cursor-pointer lg:my-0 lg:text-lg text-black/60 hover:text-black"
     >
       {title}
     </LinkScroll>
@@ -35,14 +35,14 @@ const Navbar = () => {
 
   return (
     <header className="fixed p-3 md:py-4 sm:px-4 md:px-6 md:mt-[40px] top-0 left-0 w-full h-[70px]">
-      <div className="max-w-7xl mx-auto flex items-center justify-between">
+      <div className="flex items-center justify-between mx-auto max-w-7xl">
         <div>
           <img src={logo} width={165} height={42} />
         </div>
         <div className={clsx("w-full max-lg:fixed max-lg:top-[120px] max-lg:left-0 mx-lg:w-full max-lg:bg-white max-lg:opacity-0", isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none")}>
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden max-md:px-4 max-lg:before:absolute max-lg:before:-right-64 max-lg:before:top-2/5 max-lg:before:h-[440px] max-lg:before:w-[252px] max-lg:before:bg-white max-lg:before:blur-[200px] max-lg:before:content-['']">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
-              <ul className="flex max-lg:flex-col items-start justify-start lg:items-center lg:justify-end lg:space-x-10 lg:space-y-0 space-y-6">
+              <ul className="flex items-start justify-start space-y-6 max-lg:flex-col lg:items-center lg:justify-end lg:space-x-10 lg:space-y-0">
                 <li>
                   <NavLink title="About Us" />
                 </li>
@@ -67,10 +67,10 @@ const Navbar = () => {
             </nav>
           </div>
         </div>
-        <button className="flex items-center justify-center border-2 lg:hidden z-2 size-10 border-gray-100"
+        <button className="flex items-center justify-center border-2 border-gray-100 lg:hidden z-2 size-10"
           onClick={() => setIsOpen((prev) => !prev)}
         > 
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-gray-600">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="text-gray-600 size-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
         </button>
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Element } from 'react-scroll'
+import HeroPicture from "../assets/hero-illustration.png"
+
+import CompLogo1 from "../assets/company-logo-1.png"
+import CompLogo2 from "../assets/company-logo-2.png"
+import CompLogo3 from "../assets/company-logo-3.png"
+import CompLogo4 from "../assets/company-logo-4.png"
+import CompLogo5 from "../assets/company-logo-5.png"
+import CompLogo6 from "../assets/company-logo-6.png"
+
+const Hero = () => {
+  return (
+    <section className="md:mt-[110px]">
+      <Element name="About Us" className="p-3 md:py-4 sm:px-4 md:px-6 ">
+        <div className="flex flex-col mx-auto max-w-7xl">
+          <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between h-[calc(100vh-200px)]">
+            <div className='flex flex-col items-center justify-center w-full min-h-[400px] h-full lg:items-start lg:w-1/2'>
+              <div className="w-[450px] space-y-10">
+                <h2 className="text-3xl lg:text-5xl">
+                  Navigating the digital landscape for success
+                </h2>
+                <p className="sm:text-sm md:text-md lg:text-lg">
+                  Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.
+                </p>
+                <button className="px-6 py-2 text-white rounded-lg bg-dark sm:text-sm md:text-md lg:text-lg">
+                  Book a consultation
+                </button>
+              </div>
+            </div>
+            <div className='flex items-center justify-center w-full h-full lg:justify-end lg:w-1/2'>
+              <img src={HeroPicture} alt="Hero" width={600} height={515} />
+            </div>
+          </div>
+          <div className='mt-[400px] lg:mt-0 h-[90px] pb-10 w-full mx-auto flex flex-wrap gap-6 lg:flex-nowrap justify-center lg:justify-between'>
+            <img src={CompLogo1} alt="Amazon" />
+            <img src={CompLogo2} alt="Dribbble" />
+            <img src={CompLogo3} alt="HubSpot" />
+            <img src={CompLogo4} alt="Notion" />
+            <img src={CompLogo5} alt="Netflix" />
+            <img src={CompLogo6} alt="Zoom" />
+          </div>
+        </div>
+      </Element>
+    </section>
+  )
+}
+
+export default Hero;
